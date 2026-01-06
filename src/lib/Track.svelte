a1063d60bfa3c0093708f8a998213b796e2c6326
reold <redicrafty@gmail.com>
2026-01-06 23:04:06 +0530

    / UI revamp & simplification

--- src/lib/Track.svelte
+++ src/lib/Track.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
 
-  const { track, playing = false, onPlay, onMoreActions } = $props();
+  const {
+    track,
+    playing = false,
+    onPlay,
+    onMoreActions,
+  }: {
+    track: { title: string; subtitle: string; image: string };
+    playing?: boolean;
+    onPlay?: () => void;
+    onMoreActions?: () => void;
+  } = $props();
 
   let touch = $state({
     held: false,
@@ -28,10 +38,6 @@
       if (Math.abs(dY) > 50) {
         touch.held = false;
         focused = false;
-      } else if (dX < -50) {
-        showActions = true;
-      } else {
-        showActions = false;
       }
     },
     end: () => {
@@ -44,37 +50,30 @@
     },
   };
   let focused = $state(false);
-
-  let showActions = $state(false);
 </script>
 
 <div
-  transition:fade={{ duration: 500 }}
-  class="w-full h-[10dvh] pl-1.5 min-h-[7dvh] ring-zinc-500 text-black dark:text-white overflow-hidden flex flex-row space-x-3 border-b-2 border-black/5 dark:border-white/5 py-2 select-none transition-colors duration-100 {focused
+  transition:fade={{ duration: 200 }}
+  class="w-full h-[calc(var(--spacing)*18)] pl-1.5 ring-zinc-500 text-black dark:text-white overflow-hidden flex flex-row border-b-0 border-black/5 dark:border-white/5 select-none transition-colors duration-100 {focused
     ? 'bg-black/5 dark:bg-white/5'
     : 'bg-transparent'}"
   onpointerdown={swipeController.start}
   onpointerup={swipeController.end}
+  onpointercancel={swipeController.end}
   role="button"
-  tabindex="0"
 >
   <button
-    class="p-0 m-0 max-w-[10%] my-auto transition-transform duration-200 rounded-[0.5em] overflow-hidden {focused
+    class="p-0 m-0 h-12 min-w-12 my-auto transition-transform duration-200 rounded-lg overflow-hidden {focused
       ? 'scale-105'
       : ''}"
     onclick={(e) => {
-      if (!playing) {
+      if (!playing && onPlay) {
         onPlay();
       }
     }}
   >
     {#if track.image}
-      <img
-        src={track.image}
-        alt="cover art"
-        class="max-h-[7dvh]"
-        loading="lazy"
-      />
+      <img src={track.image} alt="cover art" class="h-full" loading="lazy" />
     {:else}
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -82,7 +81,7 @@
         viewBox="0 0 24 24"
         stroke-width="1.5"
         stroke="currentColor"
-        class="size-full stroke-violet-500"
+        class="mx-auto h-10 stroke-violet-500"
       >
         <path
           stroke-linecap="round"
@@ -93,17 +92,22 @@
     {/if}
   </button>
   <button
-    class="flex flex-col overflow-x-hidden -space-y-1 justify-center text-left w-full"
+    class="flex flex-col overflow-x-hidden -space-y-0 justify-center text-left w-full"
+    onclick={(e) => {
+      if (!playing && onPlay) {
+        onPlay();
+      }
+    }}
   >
     <h2
-      class="font-medium text-base max-w-full truncate {playing
+      class="font-medium text-md max-w-full truncate {playing
         ? 'text-violet-600 dark:text-violet-500'
         : ''}"
     >
       {@html track.title}
     </h2>
     <span
-      class="font-base text-[0.7em] text-black/80 dark:text-white/80 pl-2 truncate max-w-full"
+      class="font-light text-[0.7em] text-black/80 dark:text-white/80 pl-2 truncate max-w-full"
       ><svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -122,25 +126,8 @@
     >
   </button>
 
-  <!-- <button
-        class="bg-black/5 dark:bg-white/5 text-black dark:text-white rounded-full"
-        aria-label="add track to queue"
-        onclick={onAddToQueue}
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="size-[1.2em]"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </button> -->
   <button
-    class="p-0 text-black dark:text-white rounded-full aspect-square flex flex-col items-center justify-center"
+    class="p-0 text-black dark:text-white w-16 flex flex-col items-center justify-center"
     aria-label="visit source link"
     onclick={onMoreActions}
     ><svg
@@ -148,7 +135,7 @@
       fill="none"
       viewBox="0 0 24 24"
       stroke-width="1.5"
-      class="size-[1.2em] dark:stroke-white/50 stroke-black/50"
+      class="size-[1.3em] dark:stroke-white/50 stroke-black/50"
     >
       <path
         stroke-linecap="round"
