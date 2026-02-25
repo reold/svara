d97fcde41f75534c6800a7331e802d45cc9362fe
reold <redicrafty@gmail.com>
2026-02-25 21:55:23 +0530

    Updates

---
+++ assets/Geet-BYYipJW5.js
@@ -0,0 +1,45 @@
+import{b as G,p as E,f as u,h as k,j as _,s as c,g as b,y as B,l as n,e as d,a as C,c as y,A as I,B as L,d as F,w as x,x as J,k as M,$ as N,E as O,F as R}from"./index-DKE6VTDk.js";import{h as T}from"./svelte-head-CUadUlWi.js";import{b as j}from"./input-4i5Z-8WO.js";import"./ssh-Dd9KgggB.js";var Y=u('<input type="text" placeholder="edit this line" class="bg-white/5 min-w-[70dvw] border-hidden max-w-[80dwv] focus:outline-hidden ring-1 ring-white/20 mx-2"/>'),$=u("<span> </span> ",1),A=u('<li class="text-md text-center py-2 w-full border-b border-white/10"><button class="bg-green-500/50 text-black rounded-md p-1">+</button> <button class="bg-green-500/50 text-black rounded-md p-1">dup</button> <!> <button class="bg-red-500 rounded-md text-black p-1">delete</button></li>');function K(f,v){G(v,!0);let e=E(v,"block",15),r=_(!1);var s=A();s.__click=()=>k(r,!0);var p=c(b(s),4);{var h=a=>{var l=Y();j(l,()=>e().content,t=>e(e().content=t,!0)),d(a,l)},m=a=>{var l=$(),t=y(l),i=b(t),o=c(t,1,!0);I(()=>{L(i,e().t!=-1?e().t:""),L(o,e().content)}),d(a,l)};B(p,a=>{n(r)?a(h):a(m,!1)})}d(f,s),C()}F(["click"]);var P=u('<textarea placeholder="enter raw lyrics to start with"></textarea> <button class="bg-white/10 text-blue-400">continue</button>',1),V=u('<h1 class="pl-2 bg-purple-500 font-black">Geet Builder(Standalone)</h1> <!> <div class="flex flex-col space-y-1 items-center bg-white/10 text-white"><div class="relative bg-violet-500/75 max-h-[90dvh] w-full"><ol class="max-h-[90dvh] overflow-scroll relative pb-[10dvh]"></ol> <div class="absolute bg-linear-to-b from-black/0 to-black w-full h-[10dvh] bottom-0 left-0"></div></div></div>',1);function H(f,v){G(v,!0),_(x({prefix:"kpZdEhCT04KPwdsvlctTCYY6w317Vr9j",target:"000000FF00000000000000000000000000000000000000000000000000000000"})),J(()=>{const t=JSON.parse(localStorage.getItem("temp"));t?(k(e,t,!0),console.info("using cached cSource")):console.info("not found in memory")});let e=_(x([])),r=x({rawLyrics:`മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ
+
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+മയങ്ങൂ..... നീ എന്‍ മടി മേലെ 
+അമ്പിളീ നിന്നെ പുല്‍കി അംബരം പൂകി ഞാന്‍ മേഘമായ്‌ (2)
+നിറസന്ധ്യയായ്‌ ഞാന്‍ ആരോമലേ 
+വിടര്‍ന്നെന്നില്‍ നീ ഒരു പൊന്‍താരമായ്‌ 
+ഉറങ്ങൂ... കനവു കണ്ടുണരാനായ്‌ ഉഷസണയുമ്പോള്‍ 
+
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+ആരിരോ.. ആരിരാരാരോ 
+
+എന്റെ മടിയെന്നും നിന്റെപൂമഞ്ചം 
+എന്‍മനമെന്നും നിന്‍ പൂങ്കാവനം (2)
+ഈ ജന്മത്തിലും വരും ജന്മത്തിലും 
+ഇനി എന്‍ ജീവന്‍ താരാട്ടായ്‌ ഒഴുകേണമേ 
+മധുകണം പോലെ മഞ്ഞിന്‍മണി പോലെ 
+മയങ്ങൂ... നീ ഈ ലത മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+ആരിരോ.. ആരിരാരാരോ 
+ആരിരോ.. ആരിരാരാരോ
+
+കാലമറിയാതെ ഞാന്‍ അമ്മയായ്‌ 
+കഥയറിയാതെ നീ പ്രതിഛായയായ് (2)
+നിന്‍മനമെന്‍ ധനം നിന്‍സുഖമെന്‍ സുഖം 
+ഇനി ഈ വീണ നിന്‍ രാഗമണിമാളിക 
+മധുസ്വരം പോലെ 
+മണിസ്വനം പോലെ 
+മയങ്ങൂ... ഗാന കുടം മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+അമ്പിളീ നിന്നെ പുല്‍കി അംബരം പൂകി ഞാന്‍ മേഘമായ്‌ (2)
+നിറസന്ധ്യയായ്‌ ഞാന്‍ ആരോമലേ 
+വിടര്‍ന്നെന്നില്‍ നീ ഒരു പൊന്‍താരമായ്‌ 
+ഉറങ്ങൂ... കനവു കണ്ടുണരാനായ്‌ ഉഷസണയുമ്പോള്‍ 
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ
+ആരിരോ.. ആരിരാരാരോ 
+ആരിരോ..... ആരിരാരാരോ`,parseRawLyrics:()=>{k(e,r.rawLyrics.split(`
+`).map(t=>({content:t.trim(),t:-1})).filter(t=>t.content.length>0),!0),localStorage.setItem("temp",JSON.stringify(n(e)))}});var s=V();T("1vd6nb",t=>{M(()=>{N.title="Geet Builder - Svara"})});var p=c(y(s),2);{var h=t=>{var i=P(),o=y(i),g=c(o,2);g.__click=function(...w){var S;(S=r.parseRawLyrics)==null||S.apply(this,w)},j(o,()=>r.rawLyrics,w=>r.rawLyrics=w),d(t,i)};B(p,t=>{n(e).length==0&&t(h)})}var m=c(p,2),a=b(m),l=b(a);O(l,21,()=>n(e),R,(t,i,o)=>{K(t,{get block(){return n(e)[o]},set block(g){n(e)[o]=g}})}),d(f,s),C()}F(["click"]);export{H as default};
---
+++ assets/svelte-head-CUadUlWi.js
@@ -0,0 +1,1 @@
+import{a8 as t,a9 as h,aa as o}from"./index-DKE6VTDk.js";function c(r,e){var a;a=document.head.appendChild(t());try{h(()=>e(a),o)}finally{}}export{c as h};
